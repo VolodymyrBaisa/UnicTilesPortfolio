dcfc9c9a751a60b3db79c481363f535d8e58ebbe
Volodymyr Baisa <biosvova@gmail.com>
2021-03-11 21:28:23 -0500

    Add Services size, add H2 upercase to QuoteBox, fix sizes

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,17 @@
 import React from "react";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export const Services = () => {
-    return <div></div>;
+const Services = () => {
+    return <StyledServices></StyledServices>;
 };
+
+const StyledServices = styled(motion.div)`
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    z-index: 0;
+`;
+
+export default Services;
