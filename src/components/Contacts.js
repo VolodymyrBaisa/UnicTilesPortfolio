f17d04dfbb73be6fb3e2b18020a0d306638b7626
Volodymyr Baisa <biosvova@gmail.com>
2021-03-23 16:48:42 -0400

    fix Contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
@@ -12,12 +12,22 @@ const contactsAnimation = {
 };
 
 const Contacts = ({ tAddress, tEmail, tPhone1, tPhone2 }) => {
+    const [elementWidth, setElementWidth] = useState(0);
+
+    const getElementsWidth = (el) => {
+        const imgWidth = el.target.querySelector("img")?.offsetWidth;
+        const divWidth = el.target.querySelector("div")?.offsetWidth;
+
+        return imgWidth + divWidth + 10;
+    };
+
     return (
-        <StyledContacts>
+        <StyledContacts elementWidth={elementWidth}>
             <motion.div
                 className="container"
                 variants={contactsAnimation}
                 whileTap="tap"
+                onMouseOver={(e) => setElementWidth(getElementsWidth(e))}
             >
                 <img src={mapPin} alt="Map pin icon" />
                 <div>{tAddress}</div>
@@ -26,6 +36,7 @@ const Contacts = ({ tAddress, tEmail, tPhone1, tPhone2 }) => {
                 className="container"
                 variants={contactsAnimation}
                 whileTap="tap"
+                onMouseOver={(e) => setElementWidth(getElementsWidth(e))}
             >
                 <img src={email} alt="Email icon" />
                 <div>{tEmail}</div>
@@ -34,6 +45,7 @@ const Contacts = ({ tAddress, tEmail, tPhone1, tPhone2 }) => {
                 className="container"
                 variants={contactsAnimation}
                 whileTap="tap"
+                onMouseOver={(e) => setElementWidth(getElementsWidth(e))}
             >
                 <img src={phone} alt="Phone icon" />
                 <div>{tPhone1}</div>
@@ -42,6 +54,7 @@ const Contacts = ({ tAddress, tEmail, tPhone1, tPhone2 }) => {
                 className="container"
                 variants={contactsAnimation}
                 whileTap="tap"
+                onMouseOver={(e) => setElementWidth(getElementsWidth(e))}
             >
                 <img src={phone} alt="Phone icon" />
                 <div>{tPhone2}</div>
@@ -51,8 +64,7 @@ const Contacts = ({ tAddress, tEmail, tPhone1, tPhone2 }) => {
 };
 
 const StyledContacts = styled(motion.div)`
-    display: flex;
-    flex-direction: column;
+    display: inline-block;
 
     .container {
         display: flex;
@@ -60,7 +72,6 @@ const StyledContacts = styled(motion.div)`
         align-items: center;
         position: relative;
         cursor: pointer;
-        width: auto;
         margin-bottom: 2.5rem;
 
         &:after {
@@ -76,18 +87,20 @@ const StyledContacts = styled(motion.div)`
             transition: all 0.3s ease;
         }
         &:hover:after {
-            width: 100%;
+            width: ${(props) => props.elementWidth + "px"};
         }
         img {
             width: 2.8rem;
             height: 2.8rem;
+            pointer-events: none;
         }
-
+        /*Phone*/
         div {
             margin: 0 0.5vw;
             color: #525355;
             font-weight: 800;
             font-size: 2.2rem;
+            pointer-events: none;
         }
     }
 
